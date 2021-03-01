349b2cc7556ffeb8652856f97ff382da6a5b1b59
Soroush Rabiei <soroush.rabiei@cadstar.dental>
2021-03-01 12:31:04 +0100

    Downgrade Eigen version to maintain compatibility
    * The cupoch library only works with libeigen version 3.3.7 and older,
      so we downgrade to 3.3.7 and keep this.
    
    Signed-off-by: Soroush Rabiei <soroush.rabiei@cadstar.dental>

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -1,15 +1,12 @@
 vcpkg_buildpath_length_warning(37)
 
-vcpkg_from_gitlab(
-    GITLAB_URL https://gitlab.com
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO libeigen/eigen
-    REF 3.3.9
-    SHA512 16244cc89f2e1879543232b965cbf653b3ccf10e967c8c437a41e27d8320392bdf584d8c24b8a97406ab7d1481d5154da74e0702ec1334ae6a46de83f4573a46
+    REPO eigenteam/eigen-git-mirror
+    REF 36b95962756c1fce8e29b1f8bc45967f30773c00
+    SHA512 df49657fe4818aa7733b81dc962e6764ebffcbd115e6219249aafa7853638fdc7f29273e5cc07a9684815a5dcc59e37d3a2155df5faac95c93eb5bb25c011703
     HEAD_REF master
-    PATCHES
-        disable_pkgconfig_absolute_path_check.patch
-        fix-cuda-error.patch # issue https://gitlab.com/libeigen/eigen/-/issues/1526
+    # PATCHES fix-cuda-error.patch # issue https://gitlab.com/libeigen/eigen/-/issues/1526
 )
 
 vcpkg_configure_cmake(
